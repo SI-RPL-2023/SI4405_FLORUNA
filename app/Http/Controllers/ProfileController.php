c23f2d1031c7c9fe5cb6df17d72a4acba0113fca
syeikhbagir <syeikhbagir@student.telkomuniversity.ac.id>
2023-06-03 11:03:17 +0700

    ref #20 awal

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Transaksi;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class ProfileController extends Controller
 {
@@ -11,5 +13,24 @@ class ProfileController extends Controller
     {
         $transaksi = Transaksi::where('user_id','=',auth()->user()->id)->get();
         return view('Profile', compact('transaksi'));
+    }
+
+    public function update(Request $request)
+    {
+        $user = User::find(auth()->user()->id);
+
+        $path = public_path() . '/upload/foto-profile/';
+        File::makeDirectory($path, $mode = 0777, true, true);
+
+        $foto = $request->foto->getClientOriginalName() . '-' . time() . '- Profile -'
+            . '.' . $request->foto->extension();
+        $request->foto->move($path, $foto);
+
+        $user->foto = $foto;
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->save();
+
+        return back();
     }
 }
