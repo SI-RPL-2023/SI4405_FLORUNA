fc43c5d2b05941757671f1b9782b38ff2c838afa
syeikhbagir <syeikhbagir@student.telkomuniversity.ac.id>
2023-06-04 09:08:44 +0700

    ref #20 p

--- resources/views/Profile.blade.php
+++ resources/views/Profile.blade.php
@@ -7,8 +7,13 @@
                 <h1 class="display-2 mb-3">Profile</h1>
             </div>
             <div class="row align-items-center py-5">
-                <div class="col-md-6">
-                    <img src="{{ asset('src/img/user.png') }}" class="profile-img" alt="">
+                <div class="col-md-6 text-center">
+                    @if (!auth()->user()->foto)
+                        <img src="{{ asset('src/img/user.png') }}" class="profile-img" alt="">
+                    @endif
+                    @if (auth()->user()->foto)
+                        <img src="{{ asset('upload/foto-profile/'.auth()->user()->foto) }}" class="profile-img" alt="">
+                    @endif
                 </div>
                 <div class="col-md-6">
                     <div class="p-4 fs-4">
@@ -32,7 +37,46 @@
                     </div>
                 </div>
                 <div class="text-end">
-                    <a href="#" class="btn btn-success">Update</i></a>
+                    <a href="#" class="btn btn-success" data-bs-toggle="modal"
+                        data-bs-target="#updateProfile">Update</i></a>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="updateProfile" data-bs-backdrop="static" data-bs-keyboard="false"
+                        tabindex="-1" aria-labelledby="updateProfileLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="updateProfileLabel">Update Profile</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <form action="/update/profile" method="post" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="modal-body text-start">
+                                        <div class="mb-3">
+                                            <label for="foto">Foto</label>
+                                            <input type="file" name="foto" id="foto" class="form-control">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="username">Nama</label>
+                                            <input type="text" name="username" id="username" class="form-control"
+                                                value="{{ auth()->user()->username }}">
+                                        </div>
+                                        <div class="mb-3">
+                                            <label for="email">Email</label>
+                                            <input type="email" name="email" id="email" class="form-control"
+                                                value="{{ auth()->user()->email }}">
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary"
+                                            data-bs-dismiss="modal">Close</button>
+                                        <button type="submit" class="btn btn-success">Update</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <hr class="mb-5">
@@ -56,16 +100,17 @@
                 </thead>
                 <tbody>
                     @php
-                        $i=1;
+                        $i = 1;
                     @endphp
                     @foreach ($transaksi as $data)
-                    <tr>
-                        <th scope="row">{{ $i++ }}</th>
-                        <td>{{ $data->donasi->judul }}</td>
-                        <td>{{ \Carbon\Carbon::parse($data->created_at)->format('j F Y') }}</td>
-                        <td>Rp {{ $data->jumlah }}</td>
-                        <td><a href="/donation/detail/{{ $data->donasi->id }}" class="btn btn-primary">Lihat Donasi</a></td>
-                    </tr>
+                        <tr>
+                            <th scope="row">{{ $i++ }}</th>
+                            <td>{{ $data->donasi->judul }}</td>
+                            <td>{{ \Carbon\Carbon::parse($data->created_at)->format('j F Y') }}</td>
+                            <td>Rp {{ $data->jumlah }}</td>
+                            <td><a href="/donation/detail/{{ $data->donasi->id }}" class="btn btn-primary">Lihat Donasi</a>
+                            </td>
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
